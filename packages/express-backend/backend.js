fd21c1b2663a04a8d641ff9d2388643d11652c43
Htoo Akkar Lwin <jeferryhoo@gmail.com>
2026-01-14 10:59:35 -0800

    commit for POST implement

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -76,3 +76,14 @@ app.get("/users/:id", (req, res) => {
     res.send(result);
   }
 });
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
