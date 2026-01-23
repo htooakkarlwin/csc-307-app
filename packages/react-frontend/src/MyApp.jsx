c84abc897339bb117ac80d4767707f91c995c50c
Htoo Akkar Lwin <jeferryhoo@gmail.com>
2026-01-22 20:54:45 -0800

    working frontend and backend communication

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,5 +1,6 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 import Table from "./Table";
 import Form from "./Form";
 
@@ -10,12 +11,44 @@ function MyApp() {
     const updated = characters.filter((character, i) => {
       return i !== index;
     });
-    setCharacters(updated);
+
+  setCharacters(updated);
   }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
   }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  function updateList(person) {
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function postUser(person){
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
 
   return (
     <div className="container">
